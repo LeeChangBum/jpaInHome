a89d641fc1bfcdaeade572f5f077f4d106dccc4e
LeeChangBum <lcb4843@naver.com>
2022-06-05 20:47:05 +0900

    pushed well

--- core/core/src/test/java/hello/core/BeanDefinitionTest.java
+++
@@ -1,7 +0,0 @@
-package hello.core;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class BeanDefinitionTest {
-    AnnotationConfigApplicationContext ac=new AnnotationConfigApplicationContext(AppConfig.class);
-}
